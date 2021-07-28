4335866cf45f2436bd2d6b15f84496a70d17c918
Derick Yearnsmith <d.yearnsmith@gmail.com>
2021-07-29 00:56:57 +1000

    add `BrowserRouter` to App.js and route to Goals view (goals index)

--- src/components/App.js
+++ src/components/App.js
@@ -2,13 +2,23 @@ import './App.css';
 import React from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import Nav from './Nav';
+import Goals from './Goals';
+
 
 function App() {
   return (
     <div className="App">
       <Nav />
+
+    <Router>
+      <Switch>
+        <Route exact path="/goals" component={Goals} />
+      </Switch>
+    </Router>
+
     </div>
   );
+  
 }
 
 export default App;
