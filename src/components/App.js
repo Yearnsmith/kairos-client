8a6f285b3cda66eaeb99c866c5519e59fbf500ce
Derick Yearnsmith <d.yearnsmith@gmail.com>
2021-07-29 01:04:51 +1000

    create `Calendar` component and add route for `Calendar` component in `App.js`.

--- src/components/App.js
+++ src/components/App.js
@@ -3,6 +3,7 @@ import React from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import Nav from './Nav';
 import Goals from './Goals';
+import Calendar from './Calendar'
 
 
 function App() {
@@ -13,6 +14,7 @@ function App() {
     <Router>
       <Switch>
         <Route exact path="/goals" component={Goals} />
+        <Route exact path="/calendar" component={Calendar} />
       </Switch>
     </Router>
 
---
+++ src/components/Calendar.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function Calendar() {
+    return (
+        <main>
+            <h2>Calendar</h2>
+        </main>
+    )
+}
