b2d0b16d9bb405702c869402031f9bd73438a54c
Derick Yearnsmith <d.yearnsmith@gmail.com>
2021-07-30 00:07:48 +1000

    add buttons for filter, sort, and add

--- src/components/Goals.js
+++ src/components/Goals.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import { Button } from 'semantic-ui-react'
 
 export default function Goal({termGoals}) {
 
@@ -7,7 +8,12 @@ export default function Goal({termGoals}) {
         // the Router in App.js, and wrap JSX in a fragment. But this is nice for testing.
         <main data-testid="goalsView">
             <h2>Goals</h2>
-
+            {/* this could be abstracted into a component */}
+            <div role='menu'>
+                <Button content='Filter' size='huge' compact primary />
+                <Button content='Sort' size='huge' compact primary />
+                <Button content="All" size='huge' compact primary />
+            </div>
             {/*id subject to change. This wraps all goal elements*/}
             <div id="divPane">
                 {/* map over termGoals prop and extract data. Replace div with
--- src/components/__tests__/Goals.test.js
+++ src/components/__tests__/Goals.test.js
@@ -1,9 +1,9 @@
 //import libraries
 import { render, screen, RenderResult, cleanup } from "@testing-library/react";
-const {debug} = screen
+const {debug} = screen;
 
 // import Goals component
-import Goals from '../Goals'
+import Goals from '../Goals';
 
 // create Goals data
 const termGoals = [
@@ -16,6 +16,7 @@ const termGoals = [
 // render Goals component
 beforeEach( () => render(<Goals termGoals={termGoals} />) );
 afterEach( () => cleanup() );
+
 describe("Goals component has all sub-components", () => {
     
     it("should render h2 element that has 'Goals' as text", () => {
@@ -34,12 +35,25 @@ describe("Goals component has all sub-components", () => {
         /*---------------------------*/
 
         // create array of title properties
-        const termGoalsText = termGoals.map( goal => goal.title)
+        const termGoalsText = termGoals.map( goal => goal.title);
         
         // get all title elements and create array of textContent.
-        const goalElsText = screen.getAllByRole('heading', {level:3}).map(el => el.textContent)
+        const goalElsText = screen.getAllByRole('heading', {level:3}).map(el => el.textContent);
         // match with 
-        expect(goalElsText).toEqual(termGoalsText)
+        expect(goalElsText).toEqual(termGoalsText);
 
-    })
+    });
+
+    it("should render Semantic UI Buttons for filtering and sorting'", () => {
+        // for testing desktop view
+        // const header = screen.getByRole('heading', {name: /goals/i, level: 2});
+        // const goalsMenu = screen.getByRole('menu', {name: /goal-menu/i});
+        
+        // define items that should be in filtering menu:
+        const goalsMenuItems = ['Filter','Sort','All'];
+        // get all button elements and create array of text content
+        const buttonElsText = screen.getAllByRole('button').map( el => el.textContent)
+        // compare text content of buttons to goalsMenuItems array
+        expect(buttonElsText).toEqual(goalsMenuItems);
+    });
 });
