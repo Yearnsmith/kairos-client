dd388f3a5f79b3ea9523463715507c9c64ad3857
Derick Yearnsmith <d.yearnsmith@gmail.com>
2021-07-31 12:56:31 +1000

    update data.js with real-looking data

--- src/services/data.js
+++ src/services/data.js
@@ -1,18 +1,49 @@
 
 export const data = {
     termGoals: [
-        {title:"Goal Number 1",
-        description: 'My very first goal',
-        longTermGoal: 'lifestyle',
-        timeframe: '1 week',
-        events: [
-          {title: 'event number 1'},
-          {title: 'event number 2'},
-          {title: 'event number 3'}
-        ],
-        color: 'orange'},
-        {title: "Goal Number 2", color: 'violet'},
-        {title: "Goal Number 3", color: 'yellow'},
-        {title: "Goal Number 4", color: 'teal'}
+        {
+            title:"My first goal",
+            description: 'My very first goal',
+            longTermGoal: 'lifestyle',
+            timeframe: '1 week',
+            events: [
+            {title: 'Do a thing'},
+            {title: 'Make another thing happen'},
+            {title: '...'},
+            {title: 'Profit'}
+            ],
+            color: 'teal'},
+        {
+            title: "Get a junior dev job",
+            description: 'get a foot in the door as a junior developer.',
+            longTermGoal: 'career',
+            timeframe: '2 months',
+            events: [
+                {title: 'write resume'},
+                {title: 'apply for 15 jobs'},
+                {title: 'make website'}
+            ],
+            color: 'yellow'
+        },
+        {
+            title: "Make yoga a weekly habit",
+            description: 'practice yoga once a week for 1 month',
+            longTermGoal: 'health',
+            timeframe: '1 month',
+            events: [
+                {title: 'yoga day'},
+            ],
+            color: 'red'},
+        {
+            title: "Read 3 Seminal texts on Theatre",
+            description: 'read the following books:\n- The Empty Space\n- The Theatre and it\'s double\n- Culture is the body',
+            longTermGoal: 'intellectual/education',
+            timeframe: '6 months',
+            events: [
+                {title: 'read 1 chapter of \'The Empty Space\''},
+                {title: 'read 1 chapter of \'The Theatre and it\'s double\''},
+                {title: 'read 1 chapter of \'Culture is the body\''}
+            ],
+            color: 'blue'}
     ]
   }
