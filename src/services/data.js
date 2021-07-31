395486b3660184b95c0441d5c237a7a202df83d7
Derick Yearnsmith <d.yearnsmith@gmail.com>
2021-07-31 12:11:17 +1000

    create blank reducer, stateContext files. move dummy data to data.js, import all these into App.js

---
+++ src/services/data.js
@@ -0,0 +1,18 @@
+
+export const data = {
+    termGoals: [
+        {title:"Goal Number 1",
+        description: 'My very first goal',
+        longTermGoal: 'lifestyle',
+        timeframe: '1 week',
+        events: [
+          {title: 'event number 1'},
+          {title: 'event number 2'},
+          {title: 'event number 3'}
+        ],
+        color: 'orange'},
+        {title: "Goal Number 2", color: 'violet'},
+        {title: "Goal Number 3", color: 'yellow'},
+        {title: "Goal Number 4", color: 'teal'}
+    ]
+  }
