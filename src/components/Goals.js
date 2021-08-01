694d3450d9d478881d835424f76fef2734654eb4
Derick Yearnsmith <d.yearnsmith@gmail.com>
2021-08-01 12:16:46 +1000

    conditionally render `termGoals` const (to simplify initialState in App.js), add temporary "add Goal" button until navbar is built out

--- src/components/Goals.js
+++ src/components/Goals.js
@@ -2,8 +2,7 @@ import React from 'react'
 import { Link } from 'react-router-dom'
 import { Button, List, Segment } from 'semantic-ui-react'
 
-export default function Goal({termGoals}) {
-
+export default function Goal({termGoals, addGoal}) {
     return (
         //replace div with semantic main element. We could move the element to wrap
         // the Router in App.js, and wrap JSX in a fragment. But this is nice for testing.
@@ -17,13 +16,13 @@ export default function Goal({termGoals}) {
             </div>
             {/*id subject to change. This wraps all goal elements, and gives it an accessible
             role of `list` for screen readers. selection prop gives each list item a pointer
-            on hover, and visual feedback when clicked*/}
+        on hover, and visual feedback when clicked*/}
             <List selection inverted id="goalPane">
                 {/* map over termGoals prop and extract data. Replace <List.Item> with
                 <Goal /> component once it's built out.*/}
-                {termGoals.map( goal =>
+                {termGoals && termGoals.map( goal =>
                     // Semantic UI list item
-                    <List.Item as={ Link } to={`./goals/${goal.id}`}  key={goal.title}>
+                    <List.Item as={ Link } to={`./goals/${goal.id}`}  key={ Math.random().toString(36).substr(2, 9) }>
                         {/* Semantic UI Segment allows for a coloured card-like component.
                         We could possibly modify List.Item to take a background colour if
                         we want to edit the Less styles. But this works for now.*/}
@@ -35,6 +34,8 @@ export default function Goal({termGoals}) {
                     </List.Item>
                 )}
             </List>
+            {/* temporary until navbar is built */}
+            <Button onClick={addGoal} content="Add Goal" />
         </main>
     )
 }
