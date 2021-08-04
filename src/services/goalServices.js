dc096d223705bf509dbb9cc1c2697b064bc1f036
Derick Yearnsmith <d.yearnsmith@gmail.com>
2021-08-04 10:49:55 +1000

    add goals API calls

---
+++ src/services/goalServices.js
@@ -0,0 +1,27 @@
+import kairosAPI from '../config/api'
+
+export async function getGoals(){
+    //axios 'calls' are .get, .post, ...etc.
+    const response = await kairosAPI.get("/goals");
+    return response.data;
+};
+
+export async function getGoalById(id){
+    const response = await kairosAPI.get(`/goals/${id}`);
+    return response.data;
+};
+
+export async function createGoal(data){
+    const response = await kairosAPI.post("/goals", data);
+    return response.data;
+};
+
+export async function updateGoal(data){
+    const response = await kairosAPI.put(`/goals/${data.id}`, data);
+    return response.data;
+};
+
+export async function deleteGoal(id){
+    const response = await kairosAPI.delete(`/goals/${id}`);
+    return response;
+};
