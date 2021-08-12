299d950b7b528b3dc3abf7685fbb4d6bcc66107c
Philippe Cantrel <phil@Philippes-MacBook-Air.local>
2021-08-12 21:26:32 +1000

    minor changes

--- src/utils/eventUtils.js
+++ src/utils/eventUtils.js
@@ -1,3 +1,5 @@
+
+// Credit to svestka https://stackoverflow.com/a/33018471
 export function getURL(href) {
     let urlParts = /^(?:\w+\:\/\/)?([^\/]+)(.*)$/.exec(href)
     return urlParts[1]+urlParts[2]
